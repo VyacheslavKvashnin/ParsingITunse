a6540bf21dac68d2e7e5fb5303c255aa1a826e7c
komplektan <54449303+komplektan@users.noreply.github.com>
2022-05-08 23:13:40 +0500

    Initial Commit

---
+++ ParsingITunse/ParsingITunseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParsingITunseApp.swift
+//  ParsingITunse
+//
+//  Created by Вячеслав Квашнин on 08.05.2022.
+//
+
+import SwiftUI
+
+@main
+struct ParsingITunseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
